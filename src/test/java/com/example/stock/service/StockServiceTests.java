d84fe193cbbf1033780538b2fb28018265384fe0
DaeHyeonHong <fholng@outlook.com>
2022-11-05 22:37:27 +0900

    동시성 문제 케이스 추가 RaceCondition

--- src/test/java/com/example/stock/service/StockServiceTests.java
+++ src/test/java/com/example/stock/service/StockServiceTests.java
@@ -3,11 +3,14 @@ package com.example.stock.service;
 import com.example.stock.domain.Stock;
 import com.example.stock.repository.StockRepository;
 import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class StockServiceTests {
@@ -32,6 +35,24 @@ class StockServiceTests {
     void decrease() {
         this.stockService.decrease(1L, 1L);
         final Stock stock = this.stockRepository.findById(1L).orElseThrow();
-        Assertions.assertEquals(99, stock.getQuantity());
+        assertEquals(99, stock.getQuantity());
+    }
+
+    @Test
+    void decreaseWithMultiThreadCase() throws InterruptedException {
+        int threadCount = 100;
+        final ExecutorService executorService = Executors.newFixedThreadPool(32);
+        final CountDownLatch countDownLatch = new CountDownLatch(threadCount);
+        for (int i = 0; i < threadCount; i++) {
+            try {
+                executorService.submit(() -> this.stockService.decrease(1L, 1L));
+            } finally {
+                countDownLatch.countDown();
+            }
+        }
+        countDownLatch.await();
+
+        final Stock stock = stockRepository.findById(1L).orElseThrow();
+        assertEquals(0, stock.getQuantity());
     }
 }
