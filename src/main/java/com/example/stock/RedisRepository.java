b7b40c4ba4d541c18d4d4e048a65e351f8201162
DaeHyeonHong <fholng@outlook.com>
2022-11-06 16:08:02 +0900

    LettuceLock

---
+++ src/main/java/com/example/stock/RedisRepository.java
@@ -0,0 +1,26 @@
+package com.example.stock;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.stereotype.Repository;
+import java.time.Duration;
+
+@Repository
+@RequiredArgsConstructor
+public class RedisRepository {
+    private final RedisTemplate<String, String> redisTemplate;
+
+    public Boolean lock(final Long key) {
+        return this.redisTemplate
+                .opsForValue()
+                .setIfAbsent(generateKey(key), "lock", Duration.ofMillis(3_000));
+    }
+
+    public Boolean unlock(final Long key) {
+        return this.redisTemplate.delete(generateKey(key));
+    }
+
+    private static String generateKey(final Long key) {
+        return String.valueOf(key);
+    }
+}
---
+++ src/main/java/com/example/stock/facade/LettuceLockStockFacade.java
@@ -0,0 +1,24 @@
+package com.example.stock.facade;
+
+import com.example.stock.RedisRepository;
+import com.example.stock.service.StockService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class LettuceLockStockFacade {
+    private final RedisRepository redisRepository;
+    private final StockService stockService;
+
+    public void decrease(final Long id,final Long quantity) throws InterruptedException {
+        while (Boolean.FALSE.equals(this.redisRepository.lock(id))) {
+            Thread.sleep(100);
+        }
+        try {
+            this.stockService.decrease(id,quantity);
+        }finally {
+            this.redisRepository.unlock(id);
+        }
+    }
+}
